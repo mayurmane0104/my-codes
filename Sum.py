4084ff48bc5d450b984d141004148c20548b586d
mayurmane0104 <84612181+mayurmane0104@users.noreply.github.com>
2022-02-17 13:50:23 +0530

    Create Sum.py

---
+++ Sum.py
@@ -0,0 +1,8 @@
+x = int(input())
+y = int(input())
+
+print('the sum of {0} and {1} is {2}'.format(x,y,x+y))
+
+print('the sum of {0} and {1} is {2}'.format(x,y,x*y))
+
+print('the sum of {0} and {1} is {2} and product of {0} and {1} is {3}'.format(x,y,x+y,x*y))
